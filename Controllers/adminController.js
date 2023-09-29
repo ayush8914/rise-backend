2545b8f3c5f3ecf70f4493026623a79e7d39be73
ayush <ayushgevariya8914@gmail.com>
2023-09-29 01:36:29 -0800

    ..

--- Controllers/adminController.js
+++ Controllers/adminController.js
@@ -8,13 +8,17 @@ const setfields = asyncHandler(async (req, res) => {
     const cateconfig1 = await cateconfig.findOne({});
 
     if(cateconfig1 != null){
-        cateconfig1.category.push({heading, subheading});
+        cateconfig1.category.push({heading, 
+            subheading: subheading.map(name => ({ name })),
+        });
   
         await cateconfig1.save();
         return res.status(200).json({message: 'heading and subheading added'});
     }
     else{
-        const cateconfig2 = new cateconfig({category: [ {heading, subheading}]});
+        const cateconfig2 = new cateconfig({category: [ {heading, 
+            subheading: subheading.map(name => ({ name })),
+        }]});
         console.log(cateconfig2);
         await cateconfig2.save();
         return res.status(200).json({message: 'heading and subheading added'});
