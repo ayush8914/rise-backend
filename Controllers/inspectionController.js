044746ca2aef42e532541934740874c7346bc715
ayush <ayushgevariya8914@gmail.com>
2023-10-06 02:27:05 -0800

    ......

--- Controllers/inspectionController.js
+++ Controllers/inspectionController.js
@@ -556,6 +556,6 @@ const deleteOption = asyncHandler(async (req, res) => {
 );
 
 
-const temp={}
+const temp1={}
 
 module.exports={getInspections,getReasons,getInspectionDetails, getInspectionById, addReason,createInspection,updateInspectionById,getInspection,addOptions,getOptions,deleteOption,shortdetails}
